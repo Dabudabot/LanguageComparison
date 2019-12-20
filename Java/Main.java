0878c379ec8a371f721b654ce8e0ae52802ab5e7
Daulet.Tumbayev <Daulet.Tumbayev@acronis.com>
2019-12-20 22:18:46 +0300

    fix java, add cpp

--- Java/Main.java
+++ Java/Main.java
@@ -1,37 +1,45 @@
 
 
+public class Main
+{
+	public static void main() 
+	{
+	  const short FILES_AMOUNT = 1000;
+	  
+	  long startTime = System.currentTimeMillis();
 
-public static void main() {
+	  for (short i = 0; i < FILES_AMOUNT - 1; i++)
+	  {
+		String name1 = "..//Data//" + i + ".txt";
+		BufferedReader reader1 = new BufferedReader(new FileReader(name1));
+		int number1;
+		number1 = Integer.parseInt(reader1.readLine());
+		reader1.close();
 
-  const short FILES_AMOUNT = 1000;
+		for (short j = i + 1; j < FILES_AMOUNT; j++)
+		{
+		  String name2 = "..//Data//" + j + ".txt";
+		  BufferedReader reader2 = new BufferedReader(new FileReader(name2));
+		  int number2;
+		  number2 = Integer.parseInt(reader2.readLine());
+		  reader2.close();
 
-  for (short i = 0; i < FILES_AMOUNT - 1; i++)
-  {
-    int number1;
-    BufferedReader reader1 = new BufferedReader(new FileReader("..//Data//" + i + ".txt"));
-    number1 = Integer.parseInt(reader1.readLine());
-
-    for (short j = i+1; j < FILES_AMOUNT; j++)
-    {
-      int number2;
-      BufferedReader reader2 = new BufferedReader(new FileReader("..//Data//" + j + ".txt"));
-      number2 = Integer.parseInt(reader2.readLine());
-      reader2.close();
-
-      if (number1 > number2)
-			{
-        BufferedWriter writer2 = new BufferedWriter(new FileWriter("..//Data//" + j + ".txt"));
-        writer2.write(number1);
-        writer2.close();
-
-				number1 = number2;
-      }
-    }
-
-    reader1.close();
-    BufferedWriter writer1 = new BufferedWriter(new FileWriter("..//Data//" + i + ".txt"));
-    writer1.write(number1);
-    writer1.close();
-    
-  }
+		  if (number1 > number2)
+		  {
+			BufferedWriter writer2 = new BufferedWriter(new FileWriter(name2));
+			writer2.write(number1);
+			writer2.close();
+			number1 = number2;
+		  }
+		}
+		
+		BufferedWriter writer1 = new BufferedWriter(new FileWriter(name1));
+		writer1.write(number1);
+		writer1.close();
+	  }
+	  
+	  long estimatedTime = System.currentTimeMillis() - startTime;
+	  
+	  System.out.println("elapsed time: " + estimatedTime);
+	}
 }
